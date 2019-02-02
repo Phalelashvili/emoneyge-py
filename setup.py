4585dff593164e0a835d04247dd7ec621f2ce86c
Phalelashvili <phalelashvili@protonmail.com>
2019-02-02 15:37:09 +0400

    1.0

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-version = '0.5'
+version = '1.0'
 
 setup(
     name='emoneyge-py',
@@ -9,6 +9,7 @@ setup(
     author='Spartak Phalelashvili',
     author_email='phalelashvili@protonmail.com',
     license='MIT',
+    packages=find_packages(),
     url='https://github.com/Phalelashvili/emoneyge-py',
     download_url='https://github.com/Phalelashvili/emoneyge-py/tarball/' + version,
     keywords=['emoney.ge', 'emoney', 'emoneyy', 'emoneygepy', 'emoneyge-py', 'emoney.ge automation', 'emoney automation'],
